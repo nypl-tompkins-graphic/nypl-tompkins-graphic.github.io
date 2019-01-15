bfc45a6ad13607ffac7a58b5983ef1f468893b9d
mpaulweeks <mpaulweeks@gmail.com>
2019-01-15 10:48:50 -0500

    yaml and covers

---
+++ main.js
@@ -0,0 +1,47 @@
+const renderBook = book => `
+  <div class="book">
+    <img class="cover" src="covers/${book.cover}">
+    <div class="details">
+      <div>${book.title}</div>
+      <div>${book.author}</div>
+      ${book.goodreads ? `
+        <div>
+          <a href="${book.goodreads}">Goodreads</a>
+        </div>
+      ` : ''}
+    </div>
+  </div>
+`;
+
+const compareBook = (a, b) => {
+  if (a.date < b.date){
+    return -1;
+  } else if (a.date > b.date){
+    return 1;
+  } else {
+    return 0;
+  }
+}
+
+(async () => {
+  const resp = await fetch('books.yaml');
+  const text = await resp.text();
+  const data = jsyaml.load(text);
+
+  // const today = new Date('2019-03-01');
+  const today = new Date();
+
+  document.getElementById('upcoming').innerHTML = data.books
+    .filter(b => b.date >= today)
+    .sort(compareBook)
+    .map(renderBook)
+    .join('');
+
+  document.getElementById('past').innerHTML = data.books
+    .filter(b => b.date < today)
+    .sort(compareBook)
+    .reverse()
+    .map(renderBook)
+    .join('');
+
+})()
