caeff972c74b97fb8812d219a253fdc29b4357c8
mpaulweeks <mpaulweeks@gmail.com>
2019-01-15 13:42:53 -0500

    add meta, more images, change colors, date stuff

--- main.js
+++ main.js
@@ -1,17 +1,60 @@
-const renderBook = book => `
-  <div class="book">
+const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+const Category = {
+  Current: 'current',
+  Upcoming: 'upcoming',
+  Past: 'past',
+};
+
+const dateToStr = date => {
+  const pad = (count, num) => String(num).padStart(count, '0');
+  return [
+    pad(4, date.getFullYear()),
+    pad(2, date.getMonth() + 1),
+    pad(2, date.getDate()),
+  ].join('/');
+}
+
+const renderDate = (book, category) => {
+  switch (category) {
+    case Category.Past:
+      return '';
+    case Category.Current:
+      return `<div class="date">This Month</div>`;
+    default:
+      return `
+        <div class="date">
+          ${book.displayDate}
+        </div>
+      `;
+  }
+};
+
+const renderCover = book => `
+  <div class="container">
     <img class="cover" src="covers/${book.cover}">
     <div class="details">
-      <div>${book.title}</div>
-      <div>${book.author}</div>
+      <div class="title">${book.title}</div>
+      <div class="author">${book.author}</div>
       ${book.goodreads ? `
-        <div>
+        <div class="goodreads">
           <a href="${book.goodreads}">Goodreads</a>
         </div>
       ` : ''}
     </div>
   </div>
 `;
+
+const renderBook = (book, category) => {
+  if (!book.cover){
+    return '';
+  }
+  return `
+    <div class="book">
+      ${renderDate(book, category)}
+      ${renderCover(book, category)}
+    </div>
+  `;
+};
 
 const compareBook = (a, b) => {
   if (a.date < b.date){
@@ -30,24 +73,41 @@ const compareBook = (a, b) => {
 
   // const today = new Date('2019-03-01');
   const today = new Date();
+  const yesterday = dateToStr(new Date(today - 1000*60*60*24));
 
-  const upcomingBooks = data.books
-    .filter(b => b.date >= today)
+  const books = data.books.map(book => {
+    book.year = parseFloat(book.date.split('/')[0]);
+    book.month = parseFloat(book.date.split('/')[1]);
+    book.day = parseFloat(book.date.split('/')[2]);
+    book.monthName = monthNames[book.month - 1];
+
+    const todayYear = parseFloat(today.getFullYear());
+    book.displayDate = book.year >= todayYear
+      ? `${book.monthName} ${book.day}`
+      : `${book.monthName} ${book.day}, ${book.year}`;
+
+    return book;
+  });
+
+  const upcomingBooks = books
+    .filter(b => b.date >= yesterday)
     .sort(compareBook);
-  const pastBooks = data.books
-    .filter(b => b.date < today)
+  const pastBooks = books
+    .filter(b => b.date < yesterday)
     .sort(compareBook)
     .reverse();
 
-  document.getElementById('this-month').innerHTML = renderBook(upcomingBooks[0]);
+  if (upcomingBooks.length){
+    document.getElementById('this-month').innerHTML = renderBook(upcomingBooks[0], Category.Current);
+  }
 
   document.getElementById('upcoming').innerHTML = upcomingBooks
     .filter((b, i) => i > 0)
-    .map(renderBook)
+    .map(book => renderBook(book, Category.Upcoming))
     .join('');
 
   document.getElementById('past').innerHTML = pastBooks
-    .map(renderBook)
+    .map(book => renderBook(book, Category.Past))
     .join('');
 
 })()
